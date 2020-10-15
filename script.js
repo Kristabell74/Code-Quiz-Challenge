4a6365046c80ec21a19a3abfa4ab45499d6f781a
Kristabell Johnson <bell77iam@gmail.com>
2020-10-14 22:45:29 -0600

    added code for questions and some beginning code in JS

--- script.js
+++ script.js
@@ -1,9 +1,139 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+function generateQuiz(questions, timer, counter, quizContainer, resultsContainer, submitButton) {
+    //code
+}
 
+function showQuestions(questions, quizContainer) {
+
+    var output = [];
+    var answers;
+}
+
+function showResults(questions, quizContainer, resultsContainer) {
+    //code
 }
 
 //Quiz Questions
-//A place for the results
+showQuestions(questions, quizContainer);
+
+
+
+//push button for results. A place for the results
+
+submitButton.onclick = function () {
+    showResults(questions, quizContainer, resultsContainer);
+}
+
+// Questions list 
+
+var theQuestions = [
+
+    {
+        question: 'What day is it',
+        answers: {
+            a: 'Monday',
+            b: 'Tuesday',
+            c: 'Thursday',
+
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "what is computers?",
+        answers: {
+
+            a: 'triple',
+            b: 'drink',
+            c: 'pie',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "what is computers?",
+        answers: {
+
+            a: 'triple',
+            b: 'drink',
+            c: 'pie',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "what is computers?",
+        answers: {
+
+            a: 'triple',
+            b: 'drink',
+            c: 'pie',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "what is computers?",
+        answers: {
+
+            a: 'triple',
+            b: 'drink',
+            c: 'pie',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "what is computers?",
+        answers: {
+
+            a: 'triple',
+            b: 'drink',
+            c: 'pie',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "what is computers?",
+        answers: {
+
+            a: 'triple',
+            b: 'drink',
+            c: 'pie',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "what is computers?",
+        answers: {
+
+            a: 'triple',
+            b: 'drink',
+            c: 'pie',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "what is computers?",
+        answers: {
+
+            a: 'triple',
+            b: 'drink',
+            c: 'pie',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "what is computers?",
+        answers: {
+
+            a: 'triple',
+            b: 'drink',
+            c: 'pie',
+        },
+        correctAnswer: 'c'
+    },
+
+
+
+
+
+
+]
 //A timer
 //A Counter
 //A Submit Button
