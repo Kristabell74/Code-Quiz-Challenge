87a0fe2d23c9828cfa21053c1a0991d0d5155fe6
Kristabell Johnson <bell77iam@gmail.com>
2020-10-14 23:16:17 -0600

    Push before bed

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 function generateQuiz(questions, timer, counter, quizContainer, resultsContainer, submitButton) {
+
     //code
 }
 
 function showQuestions(questions, quizContainer) {
-
+    //stores the output and answers
     var output = [];
     var answers;
 }
 
 function showResults(questions, quizContainer, resultsContainer) {
-    //code
+
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+
+    var userAnswer = '';
+    varnumcorrect = 0;
 }
+for (var i = o; i < questions.length; i++) {
+
+    userAnswer = (answerContainers[i].querySelectorAll('input[name=question' + i + ']:checked') || {}).value;
+
+    if (UserAnswer === questions[i].correctAnswer) {
+
+        numCorrect++;
+
+
+        answerContainers[i].style.color = 'purple';
+
+    }
+
+    else {
+
+        answerContainers[i].style.color = 'Orange';
+
+    }
+}
+resultsContainer.innerHtml = numCorrect + 'out of' + questions.length;
 
 //Quiz Questions
 showQuestions(questions, quizContainer);
-
 
 
 //push button for results. A place for the results
