4950fd15905abdebcb26a7d3024584eee6bd9824
Kristabell Johnson <bell77iam@gmail.com>
2020-10-18 23:28:17 -0600

    more js script and html moved around and changed

--- script.js
+++ script.js
@@ -1,102 +1,200 @@
-//Variables
-var pos = 0;
-var correct = 0;
-var quiz;
-var quiz_status;
-var question;
-var answer;
-var choices;
-var chA, chB, chC;
-var questions = [
+//starting the Quiz
+function buildQuiz() {
+    // stores the HTML output
+    const output = [];
+
+    // helps display questions
+    theQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            // stores possible answers
+            const answers = [];
+
+            for (letter in currentQuestion.answers) {
+
+                // add radio button
+                answers.push(
+                    `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+                );
+            }
+            output.push(
+                `<div class="slide">
+                  <div class="question"> ${currentQuestion.question} </div>
+                  <div class="answers"> ${answers.join("")} </div>
+                </div>`
+            );
+            // push questions and answers
+
+        }
+    );
+    quizContainer.innerHTML = output.join('');
+}
+//
+function showResults() {
+
+    // puts together the info
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // correct answers
+    let numCorrect = 0;
+
+    theQuestions.forEach((currentQuestion, questionNumber) => {
+
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question'${questionNumber}]':checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        //Correct answer gets added to total of correct answers
+        if (userAnswer === currentQuestion.correctAnswer) {
+            numCorrect++;
+
+            // correct  color light green
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else {
+            //incorrect color pink
+            answerContainers[questionNumber].style.color = 'pink';
+        }
+    });
+
+    // number of correct answers
+    resultsContainer.innerHTML = `${numCorrect} out of ${theQuestions.length}`;
+}
+
+//constant variables
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+var theQuestions = [
     // Questions list in an array
     {
         question: "Java and Javascript the same language?",
-        a: 'True',
-        b: 'False',
-        c: 'Some of it is the same',
-
-        answer: 'a'
+        answers: {
+            a: 'True',
+            b: 'False',
+            c: 'Some of it is the same'
+        },
+        correctAnswer: 'a'
     },
     {
         question: "What web browser does not support Javascript?",
-        a: 'Lynx',
-        b: 'Konqueror',
-        c: 'Opera',
-        answer: 'a'
+        answers: {
+            a: 'Lynx',
+            b: 'Konqueror',
+            c: 'Opera',
+        },
+        correctAnswer: 'a'
     },
+
     {
         question: "What does NaN stand for in Javascript?",
-        a: 'NewandNeccesary',
-        b: 'NotaNumber',
-        c: 'None of the above',
-        answer: 'b'
+        answers: {
+            a: 'NewandNeccesary',
+            b: 'NotaNumber',
+            c: 'None of the above',
+        },
+        correctAnswer: 'b'
     },
+
     {
         question: "What name was Javascript developed under?",
-        a: 'Ella',
-        b: 'SnowMan',
-        c: 'Mocha',
-        answer: 'c'
+        answer: {
+            a: 'Ella',
+            b: 'SnowMan',
+            c: 'Mocha',
+        },
+        correctAnswer: 'c'
     },
+
     {
         question: "Who built Javascript?",
-
-        a: 'Steve Jobs',
-        b: 'Brendan Eich',
-        c: 'Bill Gates',
-        answer: 'b'
+        answer: {
+            a: 'Steve Jobs',
+            b: 'Brendan Eich',
+            c: 'Bill Gates',
+        },
+        correctAnswer: 'b'
     },
+
     {
         question: "How long did it take to create Javascript?",
-        a: '10 days',
-        b: '30 days',
-        c: '20 days',
-        answer: 'a'
+        answer: {
+            a: '10 days',
+            b: '30 days',
+            c: '20 days',
+        },
+        correctAnswer: 'a'
     },
+
     {
         question: "Where did the creater of Javascript work?",
-        a: 'Microsoft',
-        b: 'Netscape',
-        c: 'Mozilla',
-        answer: 'b'
+        answer: {
+            a: 'Microsoft',
+            b: 'Netscape',
+            c: 'Mozilla',
+        },
+        correctAnswer: 'b'
     },
+
     {
         question: "Javascript was developed in what year?",
-        a: '1997',
-        b: '1996',
-        c: '1995',
-        answer: 'c'
+        answer: {
+            a: '1997',
+            b: '1996',
+            c: '1995',
+        },
+        correctAnswer: 'c'
     },
+
     {
         question: "Does Javascript have a integer data type?",
-        a: 'True',
-        b: 'False',
-        c: 'Maybe',
-        answer: 'b'
+        answer: {
+            a: 'True',
+            b: 'False',
+            c: 'Maybe',
+        },
+        correctAnswer: 'b'
     },
+
     {
         question: "What symbols do you use to make arrays?",
-        a: '{}',
-        b: '||',
-        c: '[]',
-        answer: 'c'
+        answer: {
+            a: '{}',
+            b: '||',
+            c: '[]',
+        },
+        correctAnswer: 'c'
     },
 ];
-(function () {
-    var sec = 60;
-    function startTimer() {
-        var timer = setInterval(function () {
-            sec--;
-            document.getElementById('timerDisplay').innerHTML = '00:' + sec;
-            if (sec < 0) {
-                clearInterval(timer);
-                alert("Time is up!")
-            }
-        }, 1000);
+
+//The loop for the Questions
+function theQuestions(currentQuestion, questionNumber) {
+    const answers = [];
+
+    for (letter in currentQuestion.answers) {
+
+        // radio button
+        answers.push(
+            `<label>
+          <input type="radio" name="question${questionNumber}" value="${letter}">
+          ${letter} :
+          ${currentQuestion.answers[letter]}
+        </label>`
+        );
     }
-    document.getElementById('incorrect').addEventListener('click', function () {
-        sec -= 3;
-        document.getElementById('timerDisplay').innerHTML = '00:' + sec;
-    });
-    startTimer();
-})();
+
+    // add this question and its answers to the output
+    output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+      <div class="answers"> ${answers.join('')} </div>`
+    );
+
+};
+
+
+
